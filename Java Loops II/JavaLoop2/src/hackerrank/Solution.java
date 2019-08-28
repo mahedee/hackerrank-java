c5268488b1bc565837b12346b849acbd5d7c28cc
mahedee <mahedee.hasan@gmail.com>
2019-08-28 19:29:23 +0600

    in progress

--- Java Loops II/JavaLoop2/src/hackerrank/Solution.java
+++ Java Loops II/JavaLoop2/src/hackerrank/Solution.java
@@ -1,10 +1,26 @@
 package hackerrank;
 
-public class Solution {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int value = 0;
+            for(int j = 0; j < n; j++) {
+            	
+            	value = value + a + (int)Math.pow(2, j) * b; 
+            	
+            	System.out.print(value + "  ");
+            }
+        }
+        
 
-	}
-
+        in.close();
+    }
 }
