75fff75349cf8b82df2cc236e75f8a08059d6a88
mahedee <mahedee.hasan@gmail.com>
2019-08-28 18:58:38 +0600

    add solutions

---
+++ Java Loops I/JavaLoop1/src/hackerrank/Solution.java
@@ -0,0 +1,22 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	private static final Scanner scanner = new Scanner(System.in);
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+        
+        for(int i = 1; i <= 10; i++) {
+        	System.out.println(N + " x " + i + " = " + N * i);
+        	
+        }
+
+	}
+}
---
+++ Stdinoutii/StdinoutProj/src/hackerrank/Solution.java
@@ -0,0 +1,21 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        Double d = scan.nextDouble();
+        scan.nextLine(); // to skeep new enter
+        String s = scan.nextLine();
+
+        scan.close();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
